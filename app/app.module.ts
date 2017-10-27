2a5b4baae5d03b1ec96277f607aae1c28971d0a6
Dan Nollette <nollette.dan@gmail.com>
2017-10-27 13:23:12 -0700

    Add basic functionality for adding and editing animal records.

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { FormsModule }  from '@angular/forms';
 import { AnimalListComponent }  from './animal-list.component';
-// import { EditAnimalComponent }  from './edit-animal.component';
-// import { NewAnimalComponent } from './new-animal.component';
+import { EditAnimalComponent }  from './edit-animal.component';
+import { NewAnimalComponent } from './new-animal.component';
 
 @NgModule({
   imports: [ BrowserModule,
             FormsModule ],
   declarations: [ AppComponent,
-                  // EditAnimalComponent,
-                  // NewAnimalComponent,
+                  EditAnimalComponent,
+                  NewAnimalComponent,
                   AnimalListComponent,],
   bootstrap:    [ AppComponent ]
 })
